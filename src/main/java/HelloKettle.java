fd7552006bef5385daf70cc4c40a69911ba51fa0
goja288 <goja288@gmail.com>
2015-11-06 17:30:32 -0300

    Empezando a jugar

---
+++ src/main/java/HelloKettle.java
@@ -0,0 +1,30 @@
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.NotePadMeta;
+import org.pentaho.di.core.database.Database;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.util.EnvUtil;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransHopMeta;
+import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.StepMeta;
+import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.di.trans.steps.selectvalues.SelectValuesMeta;
+import org.pentaho.di.trans.steps.tableinput.TableInputMeta;
+import org.pentaho.di.trans.steps.tableoutput.TableOutputMeta;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * Created by goja288 on 11/6/2015.
+ *
+ * Basado en http://wiki.pentaho.com/display/EAI/Pentaho+Data+Integration+-+Java+API+Examples
+ */
+public class HelloKettle {
+
+
+
+}
+
